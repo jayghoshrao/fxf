9651c37a60575583dbbcfe8bb65419c61c506c63
Jayghosh Rao <jayghoshter@gmail.com>
2025-07-22 22:21:59 +0200

    feat: menu-like selection of lines

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,7 @@
 
 #include <ftxui/dom/elements.hpp>
 #include <ftxui/component/component.hpp>
+#include <ftxui/component/component_base.hpp>
 #include <ftxui/component/screen_interactive.hpp>
 #include <ftxui/screen/string.hpp>
 
@@ -10,33 +11,28 @@
 int main() {
     using namespace ftxui;
 
+    int selector{0};
+
     auto table = io::read_table("test.csv");
 
-    std::vector<Component> components;
-    for(const auto& line : table[0])
-    {
-        components.push_back(Renderer([line] {
-            return text(line);
-        }));
-    }
+    auto menuopt = MenuOption();
+    auto menu = Menu(&table[0], &selector, menuopt) | border;
 
-    auto main_container = Container::Vertical(components);
-
-    auto with_title = Renderer(main_container, [&]{
-            return window(text("LazyViewer"), main_container->Render());
-            });
+    auto& main_container = menu;
 
     auto screen = ScreenInteractive::Fullscreen();
-    auto app = CatchEvent(with_title, [&](Event event){
+    auto app = CatchEvent(main_container, [&](Event event){
         if(event == Event::Character('q'))
         {
             screen.ExitLoopClosure()();
             return true;
         }
+
         return false;
     });
 
 
+    main_container->TakeFocus();
     screen.Loop(app);
 
     return EXIT_SUCCESS;
